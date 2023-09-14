1acf75528260002174e8b67870362e49fab33550
Ashma <ashma@zaheer.com>
2023-09-14 19:38:12 +0200

    Migration, Model and Initial Controller

---
+++ app/Models/Address.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+    protected $fillable = ['address', 'type', 'distance'];
+
+    /**
+     * Get the first available current headquarters
+     * @return Address the current headquarters if available, otherwise it makes a new one.
+     */
+    public function getHeadquarters() : Address{
+        return Address::where('type', 'headquarters')->firstOrCreate(
+            ['address' => 'Adchieve HQ - Sint Janssingel 92, 5211 DA \'s-Hertogenbosch, The Netherlands'],
+            ['type' => 'headquarters'],
+            ['distance' => 0.00]
+        );
+    }
+
+
+}
